75e89507dde7b686e4c3bc93a1fc80ed44e42151
TKaburu <traceymwendwa@gmail.com>
2024-05-21 15:52:30 +0300

    Feat: Hypermedia pagination

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+"""
+Server class
+"""
+
+import csv
+import math
+from typing import List, Dict, Any
+
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """
+    Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset."""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+                self.__dataset = dataset[1:]  # Skip the header
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Return the appropriate page of the dataset.
+        """
+        assert isinstance(page, int) and page > 0, \
+            "Page must be an integer greater than 0."
+        assert isinstance(page_size, int) and page_size > 0, \
+            "Page size must be an integer greater than 0."
+
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+
+        if start_index >= len(dataset):
+            return []
+
+        return dataset[start_index:end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+        """Return a dictionary with pagination hypermedia."""
+        dataset_page = self.get_page(page, page_size)
+        total_items = len(self.dataset())
+        total_pages = math.ceil(total_items / page_size)
+
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+            "page_size": len(dataset_page),
+            "page": page,
+            "data": dataset_page,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
+        }
